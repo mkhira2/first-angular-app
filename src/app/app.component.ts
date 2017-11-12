fe52f3b225ab6626d0c17437c133dffba0ee0566
Kenji Hirabayashi <mkhira2@gmail.com>
2017-11-12 16:10:20 -0600

    small name changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First App';
+  title = 'First Angular App';
   name = '';
 }
